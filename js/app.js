9743cd1d160ae150fbf16a157fdedbfd56445c88
DaryllCulas <culasdaryll99@gmail.com>
2024-01-17 20:49:32 +0800

    styling and functionalities fixed

--- js/app.js
+++ js/app.js
@@ -15,22 +15,10 @@ closeShopping.addEventListener('click', ()=>{
 
 let products = [
     {
-        id: 1,
-        name: 'Chicken Biryani',
-        image: 'chickenBiryani.png',
-        price: 220
-    },
-    {
         id: 2,
         name: 'Manok na pinakurat',
         image: 'manokNaPinakurat.png',
         price: 280
-    },
-    {
-        id: 3,
-        name: 'Chicken Salad',
-        image: 'chickenSalad.png',
-        price: 120
     },
     {
         id: 4,
@@ -69,64 +57,16 @@ let products = [
         price: 300
     }, 
     {
-        id: 10,
-        name: 'Pinalabong Atsara',
-        image: 'atchara.jpeg',
-        price: 50
-    },
-    {
-        id: 11, 
-        name: 'Chicken Masala Soup',
-        image: 'chickenMasalaSoup.png',
-        price: 180
-    },
-    {
-        id: 12,
-        name: 'Indian Kimchi',
-        image: 'kimchi.jpeg',
-        price: 100
-    },
-    {
-        id: 13,
-        name: 'Mashed Kamote',
-        image: 'mashKamote.jpg',
-        price: 100
-    },
-    {
         id: 14, 
         name: '10pcs Kanto Fingershots Chicken',
         image: 'kantoHotshots.png',
         price: 80
     },
     {
-        id: 15,
-        name: 'Korean Fried Chicken',
-        image: 'koreanFriedChicken.png',
-        price: 130
-    },
-    {
         id: 16,
         name: 'Gin Shembot',
         image: 'ginBulag.png',
         price: 100
-    },
-    {
-        id: 17,
-        name: 'Cuervo',
-        image: 'cuervo.jpeg',
-        price: 1400
-    },
-    {
-        id: 18,
-        name: 'Silver Patron',
-        image: 'silverPatron.png',
-        price: 3000
-    },
-    {
-        id: 19,
-        name: 'El Hombre',
-        image: 'elhombre.jpg',
-        price: 1200
     },
     {
         id: 20,
@@ -141,39 +81,15 @@ let products = [
         price: 100
     },
     {
-        id: 22,
-        name: 'Pepsi',
-        image: 'pepsi.jpeg',
-        price: 100
-    },
-    {
-        id: 23,
-        name: 'MUG Rootbeer',
-        image: 'rootbeer.jpeg',
-        price: 100
-    },
-    {
-        id: 24,
-        name: 'Sarsi',
-        image: 'sarsi.png',
-        price: 100
-    },
-    {
         id: 25,
         name: 'Gorio\'s Mineral Water',
         image: 'goriomineralwater.png',
         price: 130
     },
-    {
-        id: 26,
-        name: 'Ice Tube',
-        image: 'icetube.jpg',
-        price: 0
-    }
 ];
 let listCards  = [];
 function initApp(){
-    products.forEach((value, key) =>{
+    products.forEach((value, key) => {
         let newDiv = document.createElement('div');
         newDiv.classList.add('item');
         newDiv.innerHTML = `
